0558543c8c0d9871078d537d993e03ed562901da
Arjan Vlek <arjan.vlek@42.nl>
2016-07-23 20:44:19 +0200

    Updated ads unit id for the new app Removed ads on device information screen

--- app/src/main/java/com/arjanvlek/oxygenupdater/views/DeviceInformationFragment.java
+++ app/src/main/java/com/arjanvlek/oxygenupdater/views/DeviceInformationFragment.java
@@ -3,9 +3,7 @@ package com.arjanvlek.oxygenupdater.views;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,27 +14,18 @@ import com.arjanvlek.oxygenupdater.Model.Device;
 import com.arjanvlek.oxygenupdater.Model.DeviceInformationData;
 import com.arjanvlek.oxygenupdater.Model.SystemVersionProperties;
 import com.arjanvlek.oxygenupdater.R;
-import com.arjanvlek.oxygenupdater.Support.NetworkConnectionManager;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import java.util.List;
 
 import static com.arjanvlek.oxygenupdater.ApplicationContext.NO_OXYGEN_OS;
 
 public class DeviceInformationFragment extends AbstractFragment {
     private RelativeLayout rootView;
-    private AdView adView;
-    private NetworkConnectionManager networkConnectionManager = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         //Inflate the layout for this fragment
         rootView = (RelativeLayout) inflater.inflate(R.layout.fragment_deviceinformation, container, false);
-        if(getActivity() != null) {
-            networkConnectionManager = new NetworkConnectionManager(getActivity().getApplicationContext());
-        }
         return rootView;
     }
 
@@ -44,33 +33,34 @@ public class DeviceInformationFragment extends AbstractFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         if(isAdded()) {
-            displayDeviceInformation(null); // To fast load device information with generic / non-pretty device name.
+            displayDeviceInformation();
             new GetDevices().execute();
         }
     }
 
-    private void displayDeviceInformation(@Nullable List<Device> devices) {
-        if(isAdded()) {
-            DeviceInformationData deviceInformationData = new DeviceInformationData();
+    private void displayDeviceName(List<Device> devices) {
 
-            String deviceName = null;
+        if(isAdded()) {
+            TextView deviceNameView = (TextView) rootView.findViewById(R.id.device_information_header);
             SystemVersionProperties systemVersionProperties = getApplicationContext().getSystemVersionProperties();
 
             if (devices != null) {
                 for (Device device : devices) {
                     if (device.getProductName() != null && device.getProductName().equals(systemVersionProperties.getOxygenDeviceName())) {
-                        deviceName = device.getName();
+                        deviceNameView.setText(device.getName());
                     }
                 }
             }
+        }
+    }
 
+    private void displayDeviceInformation() {
+        if(isAdded()) {
+            DeviceInformationData deviceInformationData = new DeviceInformationData();
+            SystemVersionProperties systemVersionProperties = getApplicationContext().getSystemVersionProperties();
 
             TextView deviceNameView = (TextView) rootView.findViewById(R.id.device_information_header);
-            if (devices == null || deviceName == null) {
-                deviceNameView.setText(String.format(getString(R.string.device_information_device_name), deviceInformationData.getDeviceManufacturer(), deviceInformationData.getDeviceName()));
-            } else {
-                deviceNameView.setText(deviceName);
-            }
+            deviceNameView.setText(String.format(getString(R.string.device_information_device_name), deviceInformationData.getDeviceManufacturer(), deviceInformationData.getDeviceName()));
 
             TextView socView = (TextView) rootView.findViewById(R.id.device_information_soc_field);
             socView.setText(deviceInformationData.getSoc());
@@ -83,30 +73,23 @@ public class DeviceInformationFragment extends AbstractFragment {
                 cpuFreqView.setText(getString(R.string.device_information_unknown));
             }
 
-            long totalMemory = 0;
+            long totalMemory;
             try {
                 ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
                 ActivityManager activityManager = (ActivityManager) getActivity().getBaseContext().getSystemService(Context.ACTIVITY_SERVICE);
                 activityManager.getMemoryInfo(mi);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    totalMemory = mi.totalMem / 1048576L;
-                } else {
-                    totalMemory = 1;
-                }
+                totalMemory = mi.totalMem / 1048576L;
             } catch (Exception ignored) {
-
+                totalMemory = 0;
             }
+
             TextView memoryView = (TextView) rootView.findViewById(R.id.device_information_memory_field);
             if (totalMemory != 0) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    memoryView.setText(String.format(getString(R.string.download_size_megabyte), totalMemory));
-                } else {
-                    View memoryLabel = rootView.findViewById(R.id.device_information_memory_label);
-                    memoryLabel.setVisibility(View.GONE);
-                    memoryView.setVisibility(View.GONE);
-                }
+                memoryView.setText(String.format(getString(R.string.download_size_megabyte), totalMemory));
             } else {
-                memoryView.setText(getString(R.string.device_information_unknown));
+                View memoryLabel = rootView.findViewById(R.id.device_information_memory_label);
+                memoryLabel.setVisibility(View.GONE);
+                memoryView.setVisibility(View.GONE);
             }
 
             TextView oxygenOsVersionView = (TextView) rootView.findViewById(R.id.device_information_oxygen_os_ver_field);
@@ -138,12 +121,6 @@ public class DeviceInformationFragment extends AbstractFragment {
 
             TextView serialNumberView = (TextView) rootView.findViewById(R.id.device_information_serial_number_field);
             serialNumberView.setText(deviceInformationData.getSerialNumber());
-
-            if (networkConnectionManager != null && networkConnectionManager.checkNetworkConnection()) {
-                showAds();
-            } else {
-                hideAds();
-            }
         }
     }
 
@@ -160,69 +137,7 @@ public class DeviceInformationFragment extends AbstractFragment {
 
         @Override
         protected void onPostExecute(List<Device> devices) {
-            displayDeviceInformation(devices);
-        }
-    }
-
-    private void hideAds() {
-        if (adView != null) {
-            adView.destroy();
-        }
-    }
-
-    private void showAds() {
-
-        //TODO replace with Firebase ads
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        adView = (AdView) rootView.findViewById(R.id.device_information_banner_field);
-
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(ADS_TEST_DEVICE_ID_OWN_DEVICE)
-                .addTestDevice(ADS_TEST_DEVICE_ID_EMULATOR_1)
-                .addTestDevice(ADS_TEST_DEVICE_ID_EMULATOR_2)
-                .addTestDevice(ADS_TEST_DEVICE_ID_EMULATOR_3)
-                .build();
-
-        // Start loading the ad in the background.
-        adView.loadAd(adRequest);
-    }
-
-    /**
-     * Called when leaving the activity
-     */
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (adView != null) {
-            adView.pause();
-        }
-
-    }
-
-    /**
-     * Called when the activity enters the foreground
-     */
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) {
-            adView.resume();
-        }
-    }
-
-    /**
-     * Called before the activity is destroyed
-     */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (adView != null) {
-            adView.destroy();
+            displayDeviceName(devices);
         }
     }
 
