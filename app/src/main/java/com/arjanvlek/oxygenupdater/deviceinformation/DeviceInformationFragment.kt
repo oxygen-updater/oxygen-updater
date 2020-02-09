oxygen-updater/oxygen-updater

commit f6a8ddc140a2ad4aa17843e8d6b8d6ec9e92da70

Added informative banner in DeviceInformationFragment - Fixed an NPE due to misusing Kotlin synthetics - Added 4 strings that need to be translated

app/src/main/java/com/arjanvlek/oxygenupdater/deviceinformation/DeviceInformationFragment.kt
@@ -15,11 +15,13 @@ import com.arjanvlek.oxygenupdater.internal.Utils
 import com.arjanvlek.oxygenupdater.internal.logger.Logger
 import com.arjanvlek.oxygenupdater.internal.logger.Logger.logDebug
 import com.arjanvlek.oxygenupdater.models.Device
+import com.arjanvlek.oxygenupdater.models.DeviceOsSpec
 import com.arjanvlek.oxygenupdater.models.DeviceRequestFilter
 import com.arjanvlek.oxygenupdater.views.AbstractFragment
+import com.arjanvlek.oxygenupdater.views.MainActivity
 import kotlinx.android.synthetic.main.fragment_device_information.*
 
-class DeviceInformationFragment : AbstractFragment() {
+class DeviceInformationFragment : AbstractFragment(), MainActivity.DeviceOsSpecCheckedListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -34,6 +36,29 @@ class DeviceInformationFragment : AbstractFragment() {
 
         displayDeviceInformation()
         applicationData!!.serverConnector!!.getDevices(DeviceRequestFilter.ENABLED) { devices -> displayFormattedDeviceName(devices) }
+    }
+
+    override fun onDeviceOsSpecChecked(deviceOsSpec: DeviceOsSpec) {
+        if (isAdded) {
+            updateBannerText(deviceOsSpec)
+        }
+    }
+
+    private fun updateBannerText(deviceOsSpec: DeviceOsSpec) {
+        bannerTextView.text = getString(
+            when (deviceOsSpec) {
+                DeviceOsSpec.SUPPORTED_OXYGEN_OS -> R.string.device_information_supported_oxygen_os
+                DeviceOsSpec.CARRIER_EXCLUSIVE_OXYGEN_OS -> R.string.device_information_carrier_exclusive_oxygen_os
+                DeviceOsSpec.UNSUPPORTED_OXYGEN_OS -> R.string.device_information_unsupported_oxygen_os
+                DeviceOsSpec.UNSUPPORTED_OS -> R.string.device_information_unsupported_os
+            }
+        )
+
+        bannerLayout.visibility = View.VISIBLE
+
+        if (!deviceOsSpec.isDeviceOsSpecSupported) {
+            bannerLayout.setOnClickListener { (activity as MainActivity).displayUnsupportedDeviceOsSpecMessage(deviceOsSpec) }
+        }
     }
 
     private fun displayFormattedDeviceName(devices: List<Device>?) {

app/src/main/java/com/arjanvlek/oxygenupdater/views/MainActivity.kt
@@ -17,6 +17,7 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -57,7 +58,6 @@ import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.message_dialog_checkbox.*
 import org.joda.time.LocalDateTime
 import java.util.*
 import kotlin.math.abs
@@ -66,13 +66,18 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
 
     private lateinit var viewPager: ViewPager
     private lateinit var settingsManager: SettingsManager
-    private var serverMessageBars = ArrayList<ServerMessageBar>()
     lateinit var activityLauncher: ActivityLauncher
         private set
 
+    private var serverMessageBars = ArrayList<ServerMessageBar>()
     private var newsAd: InterstitialAd? = null
     private var downloadPermissionCallback: KotlinCallback<Boolean>? = null
     private var adView: AdView? = null
+
+    var deviceOsSpec: DeviceOsSpec? = null
+        private set
+
+    private lateinit var deviceOsSpecCheckedListener: DeviceOsSpecCheckedListener
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,16 +90,17 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
             settingsManager.deletePreference(SettingsManager.PROPERTY_SHOW_IF_SYSTEM_IS_UP_TO_DATE)
         }
 
+        val application = application as ApplicationData
+
         // Supported device check
-        if (!settingsManager.getPreference(SettingsManager.PROPERTY_IGNORE_UNSUPPORTED_DEVICE_WARNINGS, false)) {
-            val application = application as ApplicationData
+        application.serverConnector!!.getDevices(DeviceRequestFilter.ALL) { result ->
+            deviceOsSpec = Utils.checkDeviceOsSpec(application.systemVersionProperties!!, result)
+            deviceOsSpecCheckedListener.onDeviceOsSpecChecked(deviceOsSpec!!)
 
-            application.serverConnector!!.getDevices(DeviceRequestFilter.ALL) { result ->
-                val deviceOsSpec = Utils.checkDeviceOsSpec(application.systemVersionProperties!!, result)
+            val showDeviceWarningDialog = !settingsManager.getPreference(SettingsManager.PROPERTY_IGNORE_UNSUPPORTED_DEVICE_WARNINGS, false)
 
-                if (!deviceOsSpec.isDeviceOsSpecSupported) {
-                    displayUnsupportedDeviceOsSpecMessage(deviceOsSpec)
-                }
+            if (showDeviceWarningDialog && !deviceOsSpec!!.isDeviceOsSpecSupported) {
+                displayUnsupportedDeviceOsSpecMessage(deviceOsSpec!!)
             }
         }
 
@@ -400,7 +406,7 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
         adView!!.loadAd(buildAdRequest())
     }
 
-    private fun displayUnsupportedDeviceOsSpecMessage(deviceOsSpec: DeviceOsSpec) {
+    fun displayUnsupportedDeviceOsSpecMessage(deviceOsSpec: DeviceOsSpec) {
         // Do not show dialog if app was already exited upon receiving of devices from the server.
         if (isFinishing) {
             return
@@ -420,7 +426,8 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
             .setTitle(getString(R.string.unsupported_device_warning_title))
             .setMessage(getString(resourceId))
             .setPositiveButton(getString(R.string.download_error_close)) { dialog, _ ->
-                settingsManager.savePreference(SettingsManager.PROPERTY_IGNORE_UNSUPPORTED_DEVICE_WARNINGS, unsupported_device_warning_checkbox.isChecked)
+                val checkbox = checkBoxView.findViewById<CheckBox>(R.id.unsupported_device_warning_checkbox)
+                settingsManager.savePreference(SettingsManager.PROPERTY_IGNORE_UNSUPPORTED_DEVICE_WARNINGS, checkbox.isChecked)
                 dialog.dismiss()
             }
             .show()
@@ -534,7 +541,7 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
             return when (position) {
                 PAGE_NEWS -> NewsFragment()
                 PAGE_UPDATE_INFORMATION -> UpdateInformationFragment()
-                PAGE_DEVICE_INFORMATION -> DeviceInformationFragment()
+                PAGE_DEVICE_INFORMATION -> DeviceInformationFragment().also { this@MainActivity.deviceOsSpecCheckedListener = it }
                 else -> TODO()
             }
         }
@@ -552,6 +559,10 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
                 else -> null
             }
         }
+    }
+
+    interface DeviceOsSpecCheckedListener {
+        fun onDeviceOsSpecChecked(deviceOsSpec: DeviceOsSpec)
     }
 
     companion object {

app/src/main/res/layout/fragment_device_information.xml
@@ -10,6 +10,34 @@
 		android:layout_height="wrap_content"
 		android:orientation="vertical">
 
+		<LinearLayout
+			android:id="@+id/bannerLayout"
+			android:layout_width="match_parent"
+			android:layout_height="wrap_content"
+			android:background="@color/appBarBackground"
+			android:foreground="?android:selectableItemBackground"
+			android:gravity="center"
+			android:padding="@dimen/spacing_default"
+			android:visibility="gone"
+			tools:visibility="visible">
+
+			<ImageView
+				android:layout_width="wrap_content"
+				android:layout_height="wrap_content"
+				android:contentDescription="@string/icon"
+				android:src="@drawable/info" />
+
+			<TextView
+				android:id="@+id/bannerTextView"
+				android:layout_width="wrap_content"
+				android:layout_height="wrap_content"
+				android:layout_gravity="center_vertical"
+				android:layout_marginStart="@dimen/spacing_half"
+				android:layout_marginEnd="@dimen/spacing_default"
+				android:textColor="@color/white"
+				tools:text="@string/device_information_supported_oxygen_os" />
+		</LinearLayout>
+
 		<TextView
 			android:id="@+id/device_information_header"
 			android:layout_width="wrap_content"

app/src/main/res/values-ar/strings.xml
@@ -240,6 +240,12 @@
 	<string name="install_guide_zip_file_missing">"إذا كنت ترغب في الحفاظ علي جهازك ممتلكاً للروت، سيكون عليك تحديد حزمة الروت أعلاه."</string>
 
 	<!-- Device information strings -->
+	<!-- TODO: Translate these 4 strings below -->
+	<string name="device_information_supported_oxygen_os">Your device is supported</string>
+	<string name="device_information_carrier_exclusive_oxygen_os">Your device isn\'t supported, as it\'s a carrier-exclusive device</string>
+	<string name="device_information_unsupported_oxygen_os">We haven\'t added support for your new device yet</string>
+	<string name="device_information_unsupported_os">Your device isn\'t supported, as it\'s not running OxygenOS</string>
+
 	<string name="device_information_hardware_header">العتاد</string>
 	<string name="device_information_software_header">النظام</string>
 	<string name="device_information_amount_of_memory">مقدار الذاكرة العشوائية:</string>

app/src/main/res/values-da/strings.xml
@@ -277,6 +277,12 @@
 	<string name="install_guide_zip_file_missing">"Ønsker du at holde din enhed root'ed, så skal du specificere en rod-pakke ovenfor."</string>
 
 	<!-- Device information strings -->
+	<!-- TODO: Translate these 4 strings below -->
+	<string name="device_information_supported_oxygen_os">Your device is supported</string>
+	<string name="device_information_carrier_exclusive_oxygen_os">Your device isn\'t supported, as it\'s a carrier-exclusive device</string>
+	<string name="device_information_unsupported_oxygen_os">We haven\'t added support for your new device yet</string>
+	<string name="device_information_unsupported_os">Your device isn\'t supported, as it\'s not running OxygenOS</string>
+
 	<string name="device_information_hardware_header">Hardware</string>
 	<string name="device_information_software_header">Software</string>
 

app/src/main/res/values-de/strings.xml
@@ -320,6 +320,12 @@ Um das zu verifizieren, kannst du deine aktuelle Version folgendermaßen kontrol
 
 	<!-- Device information strings -->
 	<!-- TODO: Translate these lines -->
+	<!-- TODO: Translate these 4 strings below -->
+	<string name="device_information_supported_oxygen_os">Your device is supported</string>
+	<string name="device_information_carrier_exclusive_oxygen_os">Your device isn\'t supported, as it\'s a carrier-exclusive device</string>
+	<string name="device_information_unsupported_oxygen_os">We haven\'t added support for your new device yet</string>
+	<string name="device_information_unsupported_os">Your device isn\'t supported, as it\'s not running OxygenOS</string>
+
 	<string name="device_information_hardware_header">Hardware</string>
 	<string name="device_information_software_header">Software</string>
 

app/src/main/res/values-es/strings.xml
@@ -280,6 +280,12 @@
 	<string name="install_guide_zip_file_missing">"Si quieres mantener tu dispositivo rooteado, tendrás que seleccionar un paquete root."</string>
 
 	<!-- Device information strings -->
+	<!-- TODO: Translate these 4 strings below -->
+	<string name="device_information_supported_oxygen_os">Your device is supported</string>
+	<string name="device_information_carrier_exclusive_oxygen_os">Your device isn\'t supported, as it\'s a carrier-exclusive device</string>
+	<string name="device_information_unsupported_oxygen_os">We haven\'t added support for your new device yet</string>
+	<string name="device_information_unsupported_os">Your device isn\'t supported, as it\'s not running OxygenOS</string>
+
 	<string name="device_information_hardware_header">Hardware</string>
 	<string name="device_information_software_header">Software</string>
 

app/src/main/res/values-fi/strings.xml
@@ -310,6 +310,12 @@ Varmistaaksesi tämän voit tarkistaa nykyisen järjestelmän version seuraavast
 	<string name="install_guide_zip_file_missing">"Jos haluat pitää laitteesi rootattuna sinun tulee valita asennettava root paketti yläpuolelta."</string>
 
 	<!-- Device information strings -->
+	<!-- TODO: Translate these 4 strings below -->
+	<string name="device_information_supported_oxygen_os">Your device is supported</string>
+	<string name="device_information_carrier_exclusive_oxygen_os">Your device isn\'t supported, as it\'s a carrier-exclusive device</string>
+	<string name="device_information_unsupported_oxygen_os">We haven\'t added support for your new device yet</string>
+	<string name="device_information_unsupported_os">Your device isn\'t supported, as it\'s not running OxygenOS</string>
+
 	<string name="device_information_hardware_header">Laitteisto</string>
 	<string name="device_information_software_header">Ohjelmisto</string>
 

commit dcf0c4f71087b9d87ec697e8a1487f9731feb9a8

Fixed crash introduced due to changes in f6a8ddc140a2ad4aa17843e8d6b8d6ec9e92da70

app/src/main/java/com/arjanvlek/oxygenupdater/deviceinformation/DeviceInformationFragment.kt
@@ -35,7 +35,11 @@ class DeviceInformationFragment : AbstractFragment(), MainActivity.DeviceOsSpecC
         }
 
         displayDeviceInformation()
-        applicationData!!.serverConnector!!.getDevices(DeviceRequestFilter.ENABLED) { devices -> displayFormattedDeviceName(devices) }
+        applicationData!!.serverConnector!!.getDevices(DeviceRequestFilter.ALL) { devices ->
+            updateBannerText(Utils.checkDeviceOsSpec(applicationData!!.systemVersionProperties!!, devices))
+
+            displayFormattedDeviceName(devices)
+        }
     }
 
     override fun onDeviceOsSpecChecked(deviceOsSpec: DeviceOsSpec) {
@@ -57,7 +61,7 @@ class DeviceInformationFragment : AbstractFragment(), MainActivity.DeviceOsSpecC
         bannerLayout.visibility = View.VISIBLE
 
         if (!deviceOsSpec.isDeviceOsSpecSupported) {
-            bannerLayout.setOnClickListener { (activity as MainActivity).displayUnsupportedDeviceOsSpecMessage(deviceOsSpec) }
+            bannerLayout.setOnClickListener { (activity as MainActivity?)?.displayUnsupportedDeviceOsSpecMessage(deviceOsSpec) }
         }
     }
 

app/src/main/java/com/arjanvlek/oxygenupdater/views/MainActivity.kt
@@ -77,8 +77,6 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
     var deviceOsSpec: DeviceOsSpec? = null
         private set
 
-    private lateinit var deviceOsSpecCheckedListener: DeviceOsSpecCheckedListener
-
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -95,7 +93,6 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
         // Supported device check
         application.serverConnector!!.getDevices(DeviceRequestFilter.ALL) { result ->
             deviceOsSpec = Utils.checkDeviceOsSpec(application.systemVersionProperties!!, result)
-            deviceOsSpecCheckedListener.onDeviceOsSpecChecked(deviceOsSpec!!)
 
             val showDeviceWarningDialog = !settingsManager.getPreference(SettingsManager.PROPERTY_IGNORE_UNSUPPORTED_DEVICE_WARNINGS, false)
 
@@ -541,7 +538,7 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
             return when (position) {
                 PAGE_NEWS -> NewsFragment()
                 PAGE_UPDATE_INFORMATION -> UpdateInformationFragment()
-                PAGE_DEVICE_INFORMATION -> DeviceInformationFragment().also { this@MainActivity.deviceOsSpecCheckedListener = it }
+                PAGE_DEVICE_INFORMATION -> DeviceInformationFragment()
                 else -> TODO()
             }
         }