a537a9b573a70d5f4d1d82eab34366794599be1c
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2020-02-23 19:45:12 +0530

    Fixed buildConfigField errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,16 +50,16 @@ android {
         // Config for releases and testing on a real device
         // Uses the production server, and reads system properties using the OnePlus/OxygenOS specific build.prop values
         getByName("release") {
-            buildConfigField("String", "SERVER_BASE_URL", "https://oxygenupdater.com/api/v2.4/")
-            buildConfigField("String", "FAQ_SERVER_URL", "https://oxygenupdater.com/inappfaq")
-            buildConfigField("String", "NOTIFICATIONS_PREFIX", "")
-            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "ro.display.series, ro.build.product")
-            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "ro.rom.version, ro.oxygen.version, ro.build.ota.versionname")
-            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.ota")
-            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "ro.build.oemfingerprint, ro.build.fingerprint")
+            buildConfigField("String", "SERVER_BASE_URL", "\"https://oxygenupdater.com/api/v2.4/\"")
+            buildConfigField("String", "FAQ_SERVER_URL", "\"https://oxygenupdater.com/inappfaq\"")
+            buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"\"")
+            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "\"ro.display.series, ro.build.product\"")
+            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "\"ro.rom.version, ro.oxygen.version, ro.build.ota.versionname\"")
+            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.ota\"")
+            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "\"ro.build.oemfingerprint, ro.build.fingerprint\"")
             // Latter one is only used on very old OOS versions
-            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "ro.build.ab_update")
-            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "release-keys")
+            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "\"ro.build.ab_update\"")
+            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "\"release-keys\"")
             // Only devices using a properly signed (a.k.a. official) version of OxygenOS are supported
             buildConfigField("Boolean", "ADS_ARE_SUPPORTED", "true")
 
@@ -67,7 +67,7 @@ android {
 
             isMinifyEnabled = true
             isShrinkResources = true
-            isDebuggable = true
+            isDebuggable = false
 
             // to distinguish in app drawer and allow multiple builds to exist in parallel on the same device
             manifestPlaceholders = mapOf(
@@ -78,15 +78,15 @@ android {
         // Config for use during debugging and testing on an emulator
         // Uses the test server, and reads system properties using the default build.prop values present on any Android device/emulator
         getByName("debug") {
-            buildConfigField("String", "SERVER_BASE_URL", "https://test.oxygenupdater.com/api/v2.4/")
-            buildConfigField("String", "FAQ_SERVER_URL", "https://test.oxygenupdater.com/inappfaq")
-            buildConfigField("String", "NOTIFICATIONS_PREFIX", "test_")
-            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "ro.product.name")
-            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.release")
-            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.incremental")
-            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "ro.build.fingerprint")
-            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "ro.build.ab_update")
-            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "")
+            buildConfigField("String", "SERVER_BASE_URL", "\"https://test.oxygenupdater.com/api/v2.4/\"")
+            buildConfigField("String", "FAQ_SERVER_URL", "\"https://test.oxygenupdater.com/inappfaq\"")
+            buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"test_\"")
+            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "\"ro.product.name\"")
+            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.release\"")
+            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.incremental\"")
+            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "\"ro.build.fingerprint\"")
+            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "\"ro.build.ab_update\"")
+            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "\"\"")
             // During debugging, all devices are supported
             buildConfigField("Boolean", "ADS_ARE_SUPPORTED", "true") // Ads also need to be tested
 
@@ -105,15 +105,15 @@ android {
         // Config for use during debugging locally on an emulator
         // Uses localhost at port 8000, and reads system properties using the default build.prop values present on any Android device/emulator
         create("localDebug") {
-            buildConfigField("String", "SERVER_BASE_URL", "http://10.0.2.2:8000/api/v2.4/")
-            buildConfigField("String", "FAQ_SERVER_URL", "http://10.0.2.2:8000/inappfaq")
-            buildConfigField("String", "NOTIFICATIONS_PREFIX", "test_")
-            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "ro.product.name")
-            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.release")
-            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.incremental")
-            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "ro.build.fingerprint")
-            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "ro.build.ab_update")
-            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "")
+            buildConfigField("String", "SERVER_BASE_URL", "\"http://10.0.2.2:8000/api/v2.4/\"")
+            buildConfigField("String", "FAQ_SERVER_URL", "\"http://10.0.2.2:8000/inappfaq\"")
+            buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"test_\"")
+            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "\"ro.product.name\"")
+            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.release\"")
+            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.incremental\"")
+            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "\"ro.build.fingerprint\"")
+            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "\"ro.build.ab_update\"")
+            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "\"\"")
             // During debugging, all devices are supported
             buildConfigField("Boolean", "ADS_ARE_SUPPORTED", "true") // Ads also need to be tested
 
@@ -130,15 +130,15 @@ android {
         // Config for use during screenshot taking
         // Uses the real server, disables ads and reads system properties using the default build.prop values present on any Android device/emulator
         create("screenshot") {
-            buildConfigField("String", "SERVER_BASE_URL", "https://oxygenupdater.com/api/v2.4/")
-            buildConfigField("String", "FAQ_SERVER_URL", "https://oxygenupdater.com/inappfaq")
-            buildConfigField("String", "NOTIFICATIONS_PREFIX", "")
-            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "ro.product.name")
-            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.release")
-            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "ro.build.version.incremental")
-            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "ro.build.fingerprint")
-            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "ro.build.ab_update")
-            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "")
+            buildConfigField("String", "SERVER_BASE_URL", "\"https://oxygenupdater.com/api/v2.4/\"")
+            buildConfigField("String", "FAQ_SERVER_URL", "\"https://oxygenupdater.com/inappfaq\"")
+            buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"\"")
+            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "\"ro.product.name\"")
+            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.release\"")
+            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "\"ro.build.version.incremental\"")
+            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "\"ro.build.fingerprint\"")
+            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "\"ro.build.ab_update\"")
+            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "\"\"")
             // During taking screenshots, all devices are supported
             buildConfigField("Boolean", "ADS_ARE_SUPPORTED", "false") // On screenshots no ads!
 
@@ -156,15 +156,15 @@ android {
         }
         // Config for use on test devices. Uses the test server, and reads system properties using the LineageOS values from build.prop
         create("device") {
-            buildConfigField("String", "SERVER_BASE_URL", "https://test.oxygenupdater.com/api/v2.4/")
-            buildConfigField("String", "FAQ_SERVER_URL", "https://test.oxygenupdater.com/inappfaq")
-            buildConfigField("String", "NOTIFICATIONS_PREFIX", "test_")
-            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "ro.cm.device")
-            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "ro.cm.display.version")
-            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "ro.cm.display.version")
-            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "ro.build.fingerprint")
-            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "ro.build.ab_update")
-            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "")
+            buildConfigField("String", "SERVER_BASE_URL", "\"https://test.oxygenupdater.com/api/v2.4/\"")
+            buildConfigField("String", "FAQ_SERVER_URL", "\"https://test.oxygenupdater.com/inappfaq\"")
+            buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"test_\"")
+            buildConfigField("String", "DEVICE_NAME_LOOKUP_KEY", "\"ro.cm.device\"")
+            buildConfigField("String", "OS_VERSION_NUMBER_LOOKUP_KEY", "\"ro.cm.display.version\"")
+            buildConfigField("String", "OS_OTA_VERSION_NUMBER_LOOKUP_KEY", "\"ro.cm.display.version\"")
+            buildConfigField("String", "BUILD_FINGERPRINT_LOOKUP_KEY", "\"ro.build.fingerprint\"")
+            buildConfigField("String", "AB_UPDATE_LOOKUP_KEY", "\"ro.build.ab_update\"")
+            buildConfigField("String", "SUPPORTED_BUILD_FINGERPRINT_KEYS", "\"\"")
             // On test device all build types are supported
             buildConfigField("Boolean", "ADS_ARE_SUPPORTED", "true") // On test device use ads!
 
@@ -208,6 +208,8 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
     implementation(Libraries.KOTLIN_STDLIB_JDK8)
+    implementation(Libraries.KOTLIN_COROUTINES_CORE)
+    implementation(Libraries.KOTLIN_COROUTINES_ANDROID)
 
     implementation(AndroidXLibraries.APP_COMPAT)
     implementation(AndroidXLibraries.BROWSER)
