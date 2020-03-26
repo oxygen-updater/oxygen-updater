632c3dc06afcd4227e3c7178109e2c8cadbaa2cd
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2020-03-26 20:16:24 +0530

    Updated dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,10 +1,12 @@
 // https://blog.jetbrains.com/kotlin/category/releases/
-const val KOTLIN_VERSION = "1.3.70"
+// https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
+const val KOTLIN_VERSION = "1.3.71"
 
 object BuildPlugins {
     private object Versions {
         // https://developer.android.com/studio/releases/gradle-plugin
         const val BUILD_TOOLS_VERSION = "3.6.0"
+        // Maybe https://s3.amazonaws.com/fabric-artifacts/public/io/fabric/tools/gradle/maven-metadata.xml
         const val FABRIC_GRADLE_PLUGIN = "1.31.2"
         // https://developers.google.com/android/guides/releases
         const val GOOGLE_SERVICES_PLUGIN = "4.3.3"
@@ -44,13 +46,13 @@ object AndroidXLibraries {
         // https://developer.android.com/jetpack/androidx/releases/core
         const val KTX_CORE = "1.2.0"
         // https://developer.android.com/jetpack/androidx/releases/fragment
-        const val KTX_FRAGMENT = "1.2.1"
+        const val KTX_FRAGMENT = "1.2.3"
         // https://developer.android.com/jetpack/androidx/releases/lifecycle
         const val KTX_LIFECYCLE = "2.2.0"
         // https://developer.android.com/jetpack/androidx/releases/preference
         const val KTX_PREFERENCE = "1.1.0"
         // https://developer.android.com/jetpack/androidx/releases/work
-        const val KTX_WORK = "2.3.3"
+        const val KTX_WORK = "2.3.4"
         // https://developer.android.com/jetpack/androidx/releases/recyclerview
         const val RECYCLER_VIEW = "1.1.0"
     }
@@ -88,11 +90,12 @@ object Libraries {
         // https://github.com/facebook/shimmer-android/releases
         const val FACEBOOK_SHIMMER = "0.5.0"
         // https://firebase.google.com/support/release-notes/android
-        const val FIREBASE_ADS = "19.0.0"
+        // https://developers.google.com/admob/android/rel-notes
+        const val FIREBASE_ADS = "19.0.1"
         // https://firebase.google.com/support/release-notes/android
         const val FIREBASE_ANALYTICS = "17.2.3"
         // https://firebase.google.com/support/release-notes/android
-        const val FIREBASE_MESSAGING = "20.1.2"
+        const val FIREBASE_MESSAGING = "20.1.3"
         // https://github.com/bumptech/glide/releases
         const val GLIDE = "4.11.0"
         // Note: Before updating version, make sure this library's kotlin-reflect dependency's version is the same as kotlin-stdlib's version above
@@ -103,18 +106,18 @@ object Libraries {
         const val THREE_TEN_ABP = "1.2.2"
         // https://doc.insert-koin.io/#/setup/index
         // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
-        const val KOIN = "2.1.3"
+        const val KOIN = "2.1.5"
         // https://github.com/Kotlin/kotlinx.coroutines/releases
-        const val KOTLIN_COROUTINES = "1.3.4"
+        const val KOTLIN_COROUTINES = "1.3.5"
         // https://github.com/material-components/material-components-android/releases
         const val MATERIAL = "1.2.0-alpha05"
         // https://square.github.io/okhttp/changelog/
-        const val OKHTTP_LOGGING_INTERCEPTOR = "4.4.0"
+        const val OKHTTP_LOGGING_INTERCEPTOR = "4.4.1"
         // https://developers.google.com/android/guides/releases
-        const val PLAY_SERVICES_BASE = "17.1.0"
+        const val PLAY_SERVICES_BASE = "17.2.0"
         // https://github.com/square/retrofit/releases
         // https://github.com/square/retrofit/blob/master/CHANGELOG.md
-        const val RETROFIT = "2.7.2"
+        const val RETROFIT = "2.8.1"
     }
 
     const val KOTLIN_STDLIB_JDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$KOTLIN_VERSION"
@@ -174,21 +177,30 @@ object Libraries {
 
 object TestLibraries {
     private object Versions {
+        // https://developer.android.com/jetpack/androidx/releases/annotation
         const val ANNOTATION = "1.1.0"
+        // https://developer.android.com/jetpack/androidx/releases/test
         const val ESPRESSO = "3.2.0"
         const val JUNIT_EXT = "1.1.1"
+        // https://github.com/junit-team/junit4/releases
         const val JUNIT4 = "4.13"
-        const val KOIN = "2.1.3"
+        // https://doc.insert-koin.io/#/setup/index
+        // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
+        const val KOIN = "2.1.5"
+        // https://developer.android.com/jetpack/androidx/releases/test
         const val RULES = "1.2.0"
         const val RUNNER = "1.2.0"
     }
 
+    // https://github.com/junit-team/junit4/releases
     const val JUNIT4 = "junit:junit:${Versions.JUNIT4}"
     // https://developer.android.com/jetpack/androidx/releases/test
     const val JUNIT_EXT = "androidx.test.ext:junit:${Versions.JUNIT_EXT}"
 
     const val KOTLIN_TEST_JUNIT = "org.jetbrains.kotlin:kotlin-test-junit:$KOTLIN_VERSION"
 
+    // https://doc.insert-koin.io/#/setup/index
+    // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
     const val KOIN_TEST = "org.koin:koin-test:${Versions.KOIN}"
 
     // https://developer.android.com/jetpack/androidx/releases/annotation
