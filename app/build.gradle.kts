51a5754c834b2386f7443494828d20363cd9821f
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2020-06-30 01:52:06 +0530

    Release 4.3.0 https://github.com/oxygen-updater/oxygen-updater/releases/tag/oxygen-updater-4.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdkVersion(AndroidSdk.MIN)
         targetSdkVersion(AndroidSdk.TARGET)
 
-        versionCode = 67
-        versionName = "4.2.0"
+        versionCode = 68
+        versionName = "4.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
