b676a6aced7646486f040f2ed7e594948e9aa04a
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2020-07-24 09:40:54 +0530

    Added support for OnePlus Nord Also removed 8-series from ro.build.soft.version check, since these devices don't need it (ro.product.name is enough). My guess is only the 7T-series needed this, and future devices shouldn't.

--- app/src/main/java/com/oxygenupdater/models/SystemVersionProperties.kt
+++ app/src/main/java/com/oxygenupdater/models/SystemVersionProperties.kt
@@ -260,7 +260,8 @@ class SystemVersionProperties {
             "OnePlus 7T",
             "OnePlus 7T Pro",
             "OnePlus 8",
-            "OnePlus 8 Pro"
+            "OnePlus 8 Pro",
+            "OnePlus Nord"
         )
 
         // @GitHub contributors, add ro.product.name values of new OP devices *HERE*
@@ -270,9 +271,7 @@ class SystemVersionProperties {
         // Only workaround is to read ro.build.version.ota
         private val RO_BUILD_SOFT_VERSION_LOOKUP_DEVICES = listOf(
             "OnePlus7T",
-            "OnePlus7TPro",
-            "OnePlus8",
-            "OnePlus8Pro"
+            "OnePlus7TPro"
         )
     }
 }
