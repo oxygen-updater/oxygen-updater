295e26765ac9fc31cc608dc1452605110ef399c1
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2021-01-02 09:04:32 +0530

    Clicking the email button in the "About" tab now shows an app chooser Additionally, the email body now includes pre-filled information (e.g. device, update method, app version) that may help us resolve queries quicker.
    The app-chooser behaviour is experimental, and may be reverted in the future. It's intended to reduce the number of blank/spammy emails we receive.

--- app/src/main/java/com/oxygenupdater/extensions/IntentExtensions.kt
+++ app/src/main/java/com/oxygenupdater/extensions/IntentExtensions.kt
@@ -3,11 +3,21 @@ package com.oxygenupdater.extensions
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 
 /**
  * @author [Adhiraj Singh Chauhan](https://github.com/adhirajsinghchauhan)
  */
-fun Intent.withAppReferrer(context: Context) = putExtra(
-    Intent.EXTRA_REFERRER,
-    Uri.parse("android-app://${context.packageName}")
-)
+fun Intent.withAppReferrer(context: Context) = "android-app://${context.packageName}".let {
+    putExtra(
+        Intent.EXTRA_REFERRER,
+        Uri.parse(it)
+    ).apply {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            putExtra(
+                Intent.EXTRA_REFERRER_NAME,
+                it
+            )
+        }
+    }
+}
