oxygen-updater/oxygen-updater

commit ee0317c848bbf19237f12b9c33604bd4c8bf3568

feat: improved `getprop` performance and device/version detection logic
Fixes mismatch issues for 7- & 7T-series GLO/IND, 10T, Nord CE, Nord CE 2 Lite, and maybe other devices.

app/src/main/java/com/oxygenupdater/models/SystemProperties.kt
@@ -1,0 +1,46 @@
+package com.oxygenupdater.models
+
+import android.os.Build.UNKNOWN
+import java.util.concurrent.atomic.AtomicBoolean
+
+val useSystemProperties = AtomicBoolean(true)
+
+@Suppress("NOTHING_TO_INLINE")
+@Throws(UnsupportedOperationException::class)
+private inline fun invokeGet(key: String) = with(ClassAndMethod) {
+    if (!useSystemProperties.get()) throw UnsupportedOperationException("`useSystemProperties` is false")
+
+    try {
+        second?.invoke(first, key) as? String ?: UNKNOWN
+    } catch (e: Exception) {
+        useSystemProperties.set(false)
+        throw UnsupportedOperationException("`useSystemProperties` is false")
+    }
+}
+
+fun systemProperty(key: String) = invokeGet(key)
+fun systemPropertyPairs(keys: Array<String>) = keys.map {
+    it to invokeGet(it)
+}
+
+/**
+ * Reading system properties is an integral part of the app, without which it cannot perform
+ * all its device/method/version detections, which is a very basic necessity to deliver the
+ * correct OTA information to the user. These properties are read only once on a cold start.
+ *
+ * Prior to v5.10.0, we parsed the entire `getprop` (via [java.lang.Runtime.exec]) output,
+ * which was quite slow of course, even after obvious optimizations. This took anywhere
+ * between 18-30ms on debug builds running on KB2001_11_F.13 (OnePlus 8T OxygenOS 12).
+ *
+ * Using [android.os.SystemProperties] via reflection made it near-instantaneous (<1ms),
+ * which reduces noticeable jank and makes the user experience far better.
+ * **However, given that it's a `SystemApi`, this may not always work as expected (or at all)
+ * in future Android releases.**
+ */
+private val ClassAndMethod = try {
+    val clazz = Class.forName("android.os.SystemProperties")
+    clazz to clazz.getMethod("get", String::class.java)
+} catch (e: Exception) {
+    useSystemProperties.set(false)
+    null to null
+}

commit f1c2947de27e314b997f0ebfb20a7a2d22859f49

fix(system-properties): StringIndexOutOfBoundsException
Regression since ee0317c848bbf19237f12b9c33604bd4c8bf3568

app/src/main/java/com/oxygenupdater/models/SystemProperties.kt
@@ -11,7 +11,8 @@ private inline fun invokeGet(key: String) = with(ClassAndMethod) {
     if (!useSystemProperties.get()) throw UnsupportedOperationException("`useSystemProperties` is false")
 
     try {
-        second?.invoke(first, key) as? String ?: UNKNOWN
+        val value = second?.invoke(first, key) as? String
+        if (value.isNullOrEmpty()) UNKNOWN else value // ensure we never return null/empty
     } catch (e: Exception) {
         useSystemProperties.set(false)
         throw UnsupportedOperationException("`useSystemProperties` is false")

app/src/main/java/com/oxygenupdater/models/SystemVersionProperties.kt
@@ -115,7 +115,7 @@ object SystemVersionProperties {
                 val buildSoftVersion = systemProperty(BUILD_SOFT_VERSION_LOOKUP_KEY)
 
                 // Append _IN to mark device as Indian variant
-                if (buildSoftVersion[0] == 'I') oxygenDeviceName += "_IN"
+                if (buildSoftVersion.getOrNull(0) == 'I') oxygenDeviceName += "_IN"
             } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R && ONEPLUS_7_SERIES.contains(oxygenDeviceName)) {
                 // Workaround #5 (Build.PRODUCT + ro.vendor.op.india): differentiate between 7-series GLO/IND on the
                 // last two OOS11 builds (11.0.8.1 & 11.0.9.1). This property was used by system OTA to deliver the
@@ -141,7 +141,7 @@ object SystemVersionProperties {
             val euBooleanStr = pickFirstValid(BUILD_EU_LOOKUP_KEYS) { _, value -> value }
             PrefManager.putBoolean(PrefManager.PROPERTY_IS_EU_BUILD, if (euBooleanStr == UNKNOWN) {
                 val pipeline = systemProperty(VENDOR_OPLUS_REGIONMARK_LOOKUP_KEY)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ONEPLUS_7_SERIES.contains(oxygenDeviceName) || ONEPLUS_7T_SERIES.contains(oxygenDeviceName)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && (ONEPLUS_7_SERIES.contains(oxygenDeviceName) || ONEPLUS_7T_SERIES.contains(oxygenDeviceName))) {
                     // Workaround #5 (Build.PRODUCT + ro.vendor.oplus.regionmark): differentiate between GLO/IND
                     // builds for 7- & 7T-series on OOS12. This affects H.31/H.30 & F.17 builds, where the same
                     // model number is used for both regions. Not sure if future builds would also be affected.
@@ -188,7 +188,7 @@ object SystemVersionProperties {
                     val buildSoftVersion = readBuildPropItem(BUILD_SOFT_VERSION_LOOKUP_KEY, properties)
 
                     // Append _IN to mark device as Indian variant
-                    if (buildSoftVersion[0] == 'I') oxygenDeviceName += "_IN"
+                    if (buildSoftVersion.getOrNull(0) == 'I') oxygenDeviceName += "_IN"
                 } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R && ONEPLUS_7_SERIES.contains(oxygenDeviceName)) {
                     // Workaround #5 (Build.PRODUCT + ro.vendor.op.india): differentiate between 7-series GLO/IND on the
                     // last two OOS11 builds (11.0.8.1 & 11.0.9.1). This property was used by system OTA to deliver the
@@ -202,7 +202,7 @@ object SystemVersionProperties {
                 val euBooleanStr = readBuildPropItem(BUILD_EU_LOOKUP_KEYS, properties)
                 PrefManager.putBoolean(PrefManager.PROPERTY_IS_EU_BUILD, if (euBooleanStr == UNKNOWN) {
                     val pipeline = readBuildPropItem(VENDOR_OPLUS_REGIONMARK_LOOKUP_KEY, properties)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ONEPLUS_7_SERIES.contains(oxygenDeviceName) || ONEPLUS_7T_SERIES.contains(oxygenDeviceName)) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && (ONEPLUS_7_SERIES.contains(oxygenDeviceName) || ONEPLUS_7T_SERIES.contains(oxygenDeviceName))) {
                         // Workaround #5 (Build.PRODUCT + ro.vendor.oplus.regionmark): differentiate between GLO/IND
                         // builds for 7- & 7T-series on OOS12. This affects H.31/H.30 & F.17 builds, where the same
                         // model number is used for both regions. Not sure if future builds would also be affected.