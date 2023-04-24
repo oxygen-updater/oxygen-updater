655684ff04097682886a87c4d3e22ead263d1d8c
MHMDeveloper <mhmdeveloperofficial@gmail.com>
2023-04-24 12:06:36 +0500

    Upgrade AGP dependency from 7.4.2 to 8.0.0 Update default R8 processing mode Enable buildConfig build feature Preserve constant R class values

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
         classpath(BuildPlugins.GOOGLE_SERVICES_PLUGIN)
         classpath(BuildPlugins.FIREBASE_GRADLE_PLUGIN)
         classpath(BuildPlugins.KOTLIN_GRADLE_PLUGIN)
+        classpath("com.android.tools.build:gradle:8.0.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
     }
