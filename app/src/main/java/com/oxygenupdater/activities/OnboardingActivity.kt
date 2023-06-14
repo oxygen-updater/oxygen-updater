oxygen-updater/oxygen-updater

commit b79b0b5f22443ed3bb37f3d2881863ebea6bf0ca

feat(contribute): support OTA URL capture & submission on rooted devices

app/src/main/java/com/oxygenupdater/activities/OnboardingActivity.kt
@@ -1,13 +1,16 @@
 package com.oxygenupdater.activities
 
 import android.content.DialogInterface
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -17,6 +20,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.oxygenupdater.OxygenUpdater
 import com.oxygenupdater.R
 import com.oxygenupdater.databinding.ActivityOnboardingBinding
+import com.oxygenupdater.dialogs.ContributorDialogFragment
 import com.oxygenupdater.extensions.enableEdgeToEdgeUiSupport
 import com.oxygenupdater.extensions.reduceDragSensitivity
 import com.oxygenupdater.extensions.setImageResourceWithAnimationAndTint
@@ -29,10 +33,12 @@ import com.oxygenupdater.internal.settings.PrefManager
 import com.oxygenupdater.models.Device
 import com.oxygenupdater.models.DeviceOsSpec
 import com.oxygenupdater.models.SystemVersionProperties
+import com.oxygenupdater.utils.ContributorUtils
 import com.oxygenupdater.utils.Logger.logDebug
 import com.oxygenupdater.utils.Logger.logWarning
 import com.oxygenupdater.utils.SetupUtils
 import com.oxygenupdater.utils.Utils
+import com.oxygenupdater.utils.hasRootAccess
 import com.oxygenupdater.viewmodels.OnboardingViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlin.math.abs
@@ -47,6 +53,12 @@ class OnboardingActivity : BaseActivity(R.layout.activity_onboarding) {
             MainActivity.INTENT_START_PAGE,
             MainActivity.PAGE_UPDATE
         ) ?: MainActivity.PAGE_UPDATE
+    }
+
+
+    private val contributorDialog by lazy(LazyThreadSafetyMode.NONE) {
+        if (!ContributorUtils.isAtLeastQ) return@lazy null
+        ContributorDialogFragment()
     }
 
     private val onboardingViewModel by viewModel<OnboardingViewModel>()
@@ -189,7 +201,16 @@ class OnboardingActivity : BaseActivity(R.layout.activity_onboarding) {
 
         onboardingViewModel.fragmentCreated.observe(this) {
             if (it == 4) {
-                findViewById<Button>(R.id.onboardingPage4StartAppButton)?.setOnClickListener(this::onStartAppButtonClicked)
+                val visible = ContributorUtils.isAtLeastQAndPossiblyRooted
+                findViewById<CheckBox>(R.id.onboardingPage4ContributeCheckbox)?.isVisible = visible
+                findViewById<Button>(R.id.onboardingPage4MoreInfoButton)?.apply {
+                    isVisible = visible
+                    if (visible) setOnClickListener(this@OnboardingActivity::onMoreInfoButtonClicked)
+                }
+                findViewById<Button>(R.id.onboardingPage4StartAppButton)?.apply {
+                    isVisible = visible
+                    if (visible) setOnClickListener(this@OnboardingActivity::onStartAppButtonClicked)
+                }
             }
         }
     }
@@ -256,27 +277,49 @@ class OnboardingActivity : BaseActivity(R.layout.activity_onboarding) {
         }
     }
 
+    /**
+     * Show the dialog fragment only if it hasn't been added already. This
+     * can happen if the user clicks in rapid succession, which can cause
+     * the `java.lang.IllegalStateException: Fragment already added` error
+     */
+    @RequiresApi(Build.VERSION_CODES.Q)
     @Suppress("UNUSED_PARAMETER")
-    fun onStartAppButtonClicked(view: View) {
-        if (PrefManager.checkIfSetupScreenIsFilledIn()) {
-            findViewById<CheckBox>(R.id.onboardingPage4LogsCheckbox).isChecked.let {
-                PrefManager.putBoolean(
-                    PrefManager.PROPERTY_SHARE_ANALYTICS_AND_LOGS,
-                    it
-                )
+    fun onMoreInfoButtonClicked(view: View) {
+        if (!isFinishing && contributorDialog?.isAdded == false) contributorDialog!!.show(
+            supportFragmentManager,
+            ContributorDialogFragment.TAG
+        )
+    }
 
-                (application as OxygenUpdater?)?.setupCrashReporting(it)
+    @Suppress("UNUSED_PARAMETER", "MemberVisibilityCanBePrivate")
+    fun onStartAppButtonClicked(view: View) = if (PrefManager.checkIfSetupScreenIsFilledIn()) {
+        findViewById<CheckBox>(R.id.onboardingPage4LogsCheckbox).isChecked.let {
+            PrefManager.putBoolean(PrefManager.PROPERTY_SHARE_ANALYTICS_AND_LOGS, it)
+
+            (application as OxygenUpdater?)?.setupCrashReporting(it)
+        }
+
+        // If user enables OTA contribution, check if device is rooted and ask for root permission
+        if (findViewById<CheckBox>(R.id.onboardingPage4ContributeCheckbox).isChecked) {
+            Toast.makeText(this, R.string.contribute_allow_storage, Toast.LENGTH_LONG).show()
+            hasRootAccess {
+                PrefManager.putBoolean(PrefManager.PROPERTY_SETUP_DONE, true)
+                PrefManager.putBoolean(PrefManager.PROPERTY_CONTRIBUTE, true)
+                startMainActivity(startPage)
+                finish()
             }
-
+        } else {
+            // Skip shell creation and thus don't show root permission prompt
             PrefManager.putBoolean(PrefManager.PROPERTY_SETUP_DONE, true)
+            PrefManager.putBoolean(PrefManager.PROPERTY_CONTRIBUTE, false)
             startMainActivity(startPage)
             finish()
-        } else {
-            val deviceId = PrefManager.getLong(PrefManager.PROPERTY_DEVICE_ID, -1L)
-            val updateMethodId = PrefManager.getLong(PrefManager.PROPERTY_UPDATE_METHOD_ID, -1L)
-            logWarning(TAG, SetupUtils.getAsError("Setup wizard", deviceId, updateMethodId))
-            Toast.makeText(this, getString(R.string.settings_entered_incorrectly), Toast.LENGTH_LONG).show()
         }
+    } else {
+        val deviceId = PrefManager.getLong(PrefManager.PROPERTY_DEVICE_ID, -1L)
+        val updateMethodId = PrefManager.getLong(PrefManager.PROPERTY_UPDATE_METHOD_ID, -1L)
+        logWarning(TAG, SetupUtils.getAsError("Setup wizard", deviceId, updateMethodId))
+        Toast.makeText(this, getString(R.string.settings_entered_incorrectly), Toast.LENGTH_LONG).show()
     }
 
     private fun displayUnsupportedDeviceOsSpecMessage(deviceOsSpec: DeviceOsSpec) {

app/build.gradle.kts
@@ -110,7 +110,7 @@ android {
         // Uses the production server, and reads system properties using the OnePlus/OxygenOS specific build.prop values
         getByName("release") {
             buildConfigField("String", "SERVER_DOMAIN", "\"https://oxygenupdater.com/\"")
-            buildConfigField("String", "SERVER_API_BASE", "\"api/v2.6/\"")
+            buildConfigField("String", "SERVER_API_BASE", "\"api/v2.7/\"")
             buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"\"")
             buildConfigField(
                 "String[]",
@@ -144,7 +144,7 @@ android {
         // Uses the test server, and reads system properties using the default build.prop values present on any Android device/emulator
         getByName("debug") {
             buildConfigField("String", "SERVER_DOMAIN", "\"https://test.oxygenupdater.com/\"")
-            buildConfigField("String", "SERVER_API_BASE", "\"api/v2.6/\"")
+            buildConfigField("String", "SERVER_API_BASE", "\"api/v2.7/\"")
             buildConfigField("String", "NOTIFICATIONS_PREFIX", "\"test_\"")
             buildConfigField(
                 "String[]",
@@ -238,6 +238,7 @@ buildscript {
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io") // for com.github.topjohnwu.libsu
 }
 
 dependencies {
@@ -289,6 +290,10 @@ dependencies {
     implementation(Libraries.COIL)
 
     implementation(Libraries.FACEBOOK_SHIMMER)
+
+    implementation(Libraries.LIBSU_CORE)
+    implementation(Libraries.LIBSU_NIO)
+    implementation(Libraries.LIBSU_SERVICE)
 
     testImplementation(TestLibraries.JUNIT4)
     testImplementation(TestLibraries.KOTLIN_TEST_JUNIT)

app/src/main/AndroidManifest.xml
@@ -1,4 +1,5 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+<manifest
+	xmlns:android="http://schemas.android.com/apk/res/android"
 	xmlns:tools="http://schemas.android.com/tools">
 
 	<!-- Networking permissions for fetching update data -->
@@ -20,13 +21,10 @@
 	<uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
 
 	<!-- Push notification permissions -->
-	<uses-permission android:name="com.google.android.c2dm.permission.RECEIVE" />
-	<uses-permission android:name="android.permission.WAKE_LOCK" />
 	<uses-permission android:name="android.permission.VIBRATE" />
 
-	<!-- Update installation permissions -->
+	<!-- For DeviceBootOrAppUpdateReceiver -->
 	<uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
-	<uses-permission android:name="android.permission.ACCESS_SUPERUSER" />
 
 	<application
 		android:name=".OxygenUpdater"
@@ -188,6 +186,16 @@
 				<action android:name="com.google.firebase.MESSAGING_EVENT" />
 			</intent-filter>
 		</service>
+
+		<!-- Receiver to auto start RootFileService at device startup or app update -->
+		<receiver
+			android:name="com.oxygenupdater.receivers.DeviceBootOrAppUpdateReceiver"
+			android:exported="false">
+			<intent-filter>
+				<action android:name="android.intent.action.BOOT_COMPLETED" />
+				<action android:name="android.intent.action.MY_PACKAGE_REPLACED" />
+			</intent-filter>
+		</receiver>
 
 		<meta-data
 			android:name="com.google.android.gms.version"

app/src/main/java/com/oxygenupdater/OxygenUpdater.kt
@@ -25,6 +25,7 @@ import com.oxygenupdater.utils.Logger.logError
 import com.oxygenupdater.utils.MD5
 import com.oxygenupdater.utils.NotificationUtils
 import com.oxygenupdater.utils.ThemeUtils
+import com.topjohnwu.superuser.Shell
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -32,6 +33,13 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class OxygenUpdater : Application() {
+
+    init {
+        // Set settings before the main shell can be created
+        Shell.enableVerboseLogging = BuildConfig.DEBUG
+        // Accessing non-scoped storage (e.g. /data/data/) requires running in the global namespace
+        Shell.setDefaultBuilder(Shell.Builder.create().setFlags(Shell.FLAG_MOUNT_MASTER))
+    }
 
     private val networkCallback = object : ConnectivityManager.NetworkCallback() {
 

app/src/main/java/com/oxygenupdater/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oxygenupdater.activities
 import android.app.Activity
 import android.content.Intent
 import android.content.IntentSender
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,6 +12,7 @@ import android.view.View
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.annotation.IdRes
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout.LayoutParams
 import androidx.core.content.ContextCompat
@@ -38,6 +40,7 @@ import com.oxygenupdater.OxygenUpdater
 import com.oxygenupdater.OxygenUpdater.Companion.buildAdRequest
 import com.oxygenupdater.R
 import com.oxygenupdater.databinding.ActivityMainBinding
+import com.oxygenupdater.dialogs.ContributorDialogFragment
 import com.oxygenupdater.dialogs.Dialogs
 import com.oxygenupdater.dialogs.MessageDialog
 import com.oxygenupdater.dialogs.ServerMessagesDialogFragment
@@ -54,10 +57,12 @@ import com.oxygenupdater.models.Device
 import com.oxygenupdater.models.DeviceOsSpec
 import com.oxygenupdater.models.ServerMessage
 import com.oxygenupdater.models.ServerStatus
+import com.oxygenupdater.utils.ContributorUtils
 import com.oxygenupdater.utils.Logger.logWarning
 import com.oxygenupdater.utils.SetupUtils
 import com.oxygenupdater.utils.Utils
 import com.oxygenupdater.utils.Utils.checkPlayServices
+import com.oxygenupdater.utils.hasRootAccess
 import com.oxygenupdater.viewmodels.BillingViewModel
 import com.oxygenupdater.viewmodels.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -77,6 +82,11 @@ class MainActivity : BaseActivity(R.layout.activity_main), Toolbar.OnMenuItemCli
 
     private val serverMessagesDialog by lazy(LazyThreadSafetyMode.NONE) {
         ServerMessagesDialogFragment()
+    }
+
+    private val contributorDialog by lazy(LazyThreadSafetyMode.NONE) {
+        if (!ContributorUtils.isAtLeastQ) return@lazy null
+        ContributorDialogFragment(true)
     }
 
     private val noConnectionSnackbar by lazy(LazyThreadSafetyMode.NONE) {
@@ -161,6 +171,20 @@ class MainActivity : BaseActivity(R.layout.activity_main), Toolbar.OnMenuItemCli
         }
 
         setupLiveDataObservers()
+
+        // Offer contribution to users from app versions below v2.4.0 and v5.10.1
+        if (ContributorUtils.isAtLeastQAndPossiblyRooted && !PrefManager.contains(PrefManager.PROPERTY_CONTRIBUTE)) {
+            showContributorDialog()
+        }
+
+        binding.toolbar.menu.findItem(R.id.action_contribute).isVisible = ContributorUtils.isAtLeastQAndPossiblyRooted
+
+        // TODO(root): move this to the proper place
+        hasRootAccess {
+            if (!it) return@hasRootAccess ContributorUtils.stopDbCheckingProcess(this)
+
+            ContributorUtils.startDbCheckingProcess(this)
+        }
     }
 
     override fun onResume() = super.onResume().also {
@@ -196,6 +220,11 @@ class MainActivity : BaseActivity(R.layout.activity_main), Toolbar.OnMenuItemCli
     override fun onMenuItemClick(item: MenuItem) = when (item.itemId) {
         R.id.action_announcements -> showServerMessagesDialog().let { true }
         R.id.action_mark_articles_read -> mainViewModel.notifyMenuClicked(item.itemId).let { true }
+        R.id.action_contribute -> {
+            if (ContributorUtils.isAtLeastQAndPossiblyRooted) showContributorDialog() else item.isVisible = false
+            true
+        }
+
         else -> super.onOptionsItemSelected(item)
     }
 
@@ -514,6 +543,19 @@ class MainActivity : BaseActivity(R.layout.activity_main), Toolbar.OnMenuItemCli
                 ServerMessagesDialogFragment.TAG
             )
         }
+    }
+
+    /**
+     * Show the dialog fragment only if it hasn't been added already. This
+     * can happen if the user clicks in rapid succession, which can cause
+     * the `java.lang.IllegalStateException: Fragment already added` error
+     */
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun showContributorDialog() {
+        if (!isFinishing && contributorDialog?.isAdded == false) contributorDialog!!.show(
+            supportFragmentManager,
+            ContributorDialogFragment.TAG
+        )
     }
 
     fun updateToolbarForPage(@IdRes pageId: Int, subtitle: CharSequence? = null) {

app/src/main/java/com/oxygenupdater/apis/ServerApi.kt
@@ -1,5 +1,6 @@
 package com.oxygenupdater.apis
 
+import androidx.collection.ArrayMap
 import com.oxygenupdater.models.Device
 import com.oxygenupdater.models.InAppFaq
 import com.oxygenupdater.models.InstallGuidePage
@@ -90,6 +91,20 @@ interface ServerApi {
         @Path("updateMethodId") updateMethodId: Long,
         @Path("pageNumber") pageNumber: Int
     ): Response<InstallGuidePage>
+
+    /**
+     * @param body includes the following fields:
+     * * `rows: List<ArrayMap<String, Any?>>`,
+     * * `otaVersion: String`,
+     * * `isEuBuild: Boolean`,
+     * * `appVersion: String`,
+     * * `deviceName: String`,
+     * * `actualDeviceName: String`,
+     */
+    @POST("submit-update-url")
+    suspend fun submitOtaDbRows(
+        @Body body: ArrayMap<String, Any>
+    ): Response<ServerPostResult>
 
     /**
      * @param body includes the following fields:

app/src/main/java/com/oxygenupdater/dao/SubmittedUpdateFileDao.kt
@@ -2,6 +2,7 @@ package com.oxygenupdater.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.oxygenupdater.models.SubmittedUpdateFile
 
@@ -11,9 +12,9 @@ import com.oxygenupdater.models.SubmittedUpdateFile
 @Dao
 interface SubmittedUpdateFileDao {
 
-    @Query("SELECT COUNT(name) > 0 FROM submitted_update_file WHERE name = :filename")
-    fun isFileAlreadySubmitted(filename: String?): Boolean
+    @Query("SELECT EXISTS(SELECT 1 FROM submitted_update_file WHERE name = :url LIMIT 1)")
+    fun isUrlAlreadySubmitted(url: String?): Boolean
 
-    @Insert
-    fun insert(submittedUpdateFile: SubmittedUpdateFile)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg submittedUpdateFile: SubmittedUpdateFile)
 }

app/src/main/java/com/oxygenupdater/dialogs/ContributorDialogFragment.kt
@@ -1,0 +1,96 @@
+package com.oxygenupdater.dialogs
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.oxygenupdater.R
+import com.oxygenupdater.databinding.BottomSheetContributorBinding
+import com.oxygenupdater.internal.settings.PrefManager
+import com.oxygenupdater.internal.settings.PrefManager.PROPERTY_CONTRIBUTE
+import com.oxygenupdater.utils.ContributorUtils
+import com.oxygenupdater.utils.hasRootAccess
+
+/**
+ * Wrapper around [BottomSheetDialogFragment]
+ */
+@RequiresApi(Build.VERSION_CODES.Q) // same as RootFileService
+class ContributorDialogFragment(
+    private val showEnrollment: Boolean = false
+) : BottomSheetDialogFragment() {
+
+    override fun onCreateDialog(
+        savedInstanceState: Bundle?
+    ) = super.onCreateDialog(savedInstanceState).apply {
+        setOnShowListener {
+            // Open up the dialog fully by default
+            (this as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+    /** Only valid between `onCreateView` and `onDestroyView` */
+    private var binding: BottomSheetContributorBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = BottomSheetContributorBinding.inflate(inflater, container, false).run {
+        binding = this
+        root
+    }.also {
+        // This behaviour gets updated when checkbox value changes
+        // But we still need the default behaviour to be applied
+        isCancelable = true
+    }
+
+    override fun onDestroyView() = super.onDestroyView().also {
+        binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = setupViews()
+
+    private fun setupViews() {
+        val initialValue = PrefManager.getBoolean(PROPERTY_CONTRIBUTE, false)
+
+        binding?.negativeButton?.run {
+            isVisible = showEnrollment
+            setOnClickListener {
+                binding?.contributeCheckbox?.isChecked = initialValue.also { dismiss() }
+            }
+        }
+
+        binding?.positiveButton?.run {
+            isVisible = showEnrollment
+            setOnClickListener {
+                if (binding?.contributeCheckbox?.isChecked == true) hasRootAccess {
+                    if (it) ContributorUtils.flushSettings(context, true).also {
+                        dismiss()
+                    } else Toast.makeText(context, R.string.contribute_allow_storage, Toast.LENGTH_LONG).show()
+                } else ContributorUtils.flushSettings(context, false).also {
+                    dismiss()
+                }
+            }
+        }
+
+        binding?.contributeCheckbox?.run {
+            isVisible = showEnrollment
+            isChecked = initialValue
+
+            setOnCheckedChangeListener { _, isChecked ->
+                // Allow dismissing the dialog only if value hasn't changed
+                isCancelable = !showEnrollment || initialValue == isChecked
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "ContributorDialogFragment"
+    }
+}

commit d874cba4d4bd44a657f1436de802d3c8f9d1ccc6

fix(onboarding): start button wasn't shown on non-rooted or Android < 10/Q devices
This was a regression since b79b0b5f22443ed3bb37f3d2881863ebea6bf0ca

app/src/main/java/com/oxygenupdater/activities/OnboardingActivity.kt
@@ -207,10 +207,7 @@ class OnboardingActivity : BaseActivity(R.layout.activity_onboarding) {
                     isVisible = visible
                     if (visible) setOnClickListener(this@OnboardingActivity::onMoreInfoButtonClicked)
                 }
-                findViewById<Button>(R.id.onboardingPage4StartAppButton)?.apply {
-                    isVisible = visible
-                    if (visible) setOnClickListener(this@OnboardingActivity::onStartAppButtonClicked)
-                }
+                findViewById<Button>(R.id.onboardingPage4StartAppButton)?.setOnClickListener(this::onStartAppButtonClicked)
             }
         }
     }