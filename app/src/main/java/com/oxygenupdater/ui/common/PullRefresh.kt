ec8d87042924513cabaf6bc3232ffa44055da3b6
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2023-11-16 08:38:58 +0530

    Updated dependencies
    Android Gradle Plugin  8.1.3         -> 8.1.4
    Google Services Plugin 4.3.15        -> 4.4.0
    
    Compose                1.6.0-alpha08 -> 1.6.0-beta01 (fixes https://issuetracker.google.com/issues/306657218)
    Compose M3             1.2.0-alpha10 -> 1.2.0-alpha11 (replaced M2-copied PullRefresh with new M3 PullToRefresh)
    Activity-Compose       1.8.0         -> 1.8.1
    
    Google Play Billing    6.0.1         -> 6.1.0
    AndroidX Browser       1.6.0         -> 1.7.0
    AndroidX Lifecycle     2.7.0-beta01  -> 2.7.0-rc01
    
    Full APK size did not change noticeably

--- app/src/main/java/com/oxygenupdater/ui/common/PullRefresh.kt
+++ app/src/main/java/com/oxygenupdater/ui/common/PullRefresh.kt
@@ -1,18 +1,21 @@
 package com.oxygenupdater.ui.common
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.oxygenupdater.ui.RefreshAwareState
-import com.oxygenupdater.ui.pullrefresh.PullRefreshIndicator
-import com.oxygenupdater.ui.pullrefresh.pullRefresh
-import com.oxygenupdater.ui.pullrefresh.rememberPullRefreshState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,15 +28,39 @@ fun <T> PullRefresh(
     val refreshing = state.refreshing
     if (refreshing && shouldShowProgressIndicator(state.data)) Box(modifierMaxSize, Alignment.Center) {
         CircularProgressIndicator(Modifier.size(64.dp), strokeWidth = 6.dp)
-    } else rememberPullRefreshState(refreshing, onRefresh).let {
-        Box(Modifier.pullRefresh(it)) {
+    } else rememberPullToRefreshState().let {
+        /**
+         * If our own refresh [state] is false, M3's PullToRefreshState needs to
+         * be signalled. Note: we're deliberately not handling the `true` case,
+         * i.e. call `startRefresh`, because that would fire off [onRefresh].
+         *
+         * We don't want that, because ViewModels already fetch data on init,
+         * and we'd get a duplicate network request if we do it here too.
+         *
+         * The downside is that indicator isn't shown initially.
+         */
+        if (!refreshing) LaunchedEffect(Unit) { it.endRefresh() }
+        if (it.isRefreshing) LaunchedEffect(Unit) { onRefresh() }
+
+        Box(Modifier.nestedScroll(it.nestedScrollConnection)) {
             content()
 
-            PullRefreshIndicator(
-                refreshing = refreshing,
+            PullToRefreshContainer(
                 state = it,
                 contentColor = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.align(Alignment.TopCenter)
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .graphicsLayer {
+                        if (it.isRefreshing) return@graphicsLayer
+
+                        // Grow when pulling down. This is required, otherwise the indicator
+                        // would always be visible even when not refreshing.
+                        val scale = LinearOutSlowInEasing
+                            .transform(it.progress)
+                            .coerceIn(0f, 1f)
+                        scaleX = scale
+                        scaleY = scale
+                    }
             )
         }
     }
