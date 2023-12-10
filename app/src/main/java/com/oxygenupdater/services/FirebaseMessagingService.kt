b744d940b01d356b61dcdd8a7f1764ae54ed5b25
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2023-12-10 09:19:34 +0530

    feat(fcm): resubscribe notification topic on registration token change

--- app/src/main/java/com/oxygenupdater/services/FirebaseMessagingService.kt
+++ app/src/main/java/com/oxygenupdater/services/FirebaseMessagingService.kt
@@ -10,6 +10,7 @@ import com.google.firebase.messaging.RemoteMessage
 import com.oxygenupdater.enums.NotificationElement
 import com.oxygenupdater.enums.NotificationType
 import com.oxygenupdater.internal.settings.PrefManager
+import com.oxygenupdater.utils.FcmUtils
 import com.oxygenupdater.utils.logDebug
 import com.oxygenupdater.utils.logError
 import com.oxygenupdater.workers.DisplayDelayedNotificationWorker
@@ -19,13 +20,12 @@ import kotlin.random.Random
 
 class FirebaseMessagingService : FirebaseMessagingService() {
 
-    private val random = Random.Default
-
     private val workManager by inject<WorkManager>()
 
     override fun onNewToken(token: String) {
         logDebug(TAG, "Received new Firebase token: $token")
         PrefManager.putString(PrefManager.KeyFirebaseToken, token)
+        FcmUtils.resubscribe()
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) = try {
@@ -48,7 +48,7 @@ class FirebaseMessagingService : FirebaseMessagingService() {
             if (!it) return@let // new version notifications should be shown immediately
 
             val serverSpecifiedDelay = PrefManager.getInt(PrefManager.KeyNotificationDelayInSeconds, 10)
-            val delay = random.nextLong(1, serverSpecifiedDelay.coerceAtLeast(2).toLong())
+            val delay = Random.nextLong(1, serverSpecifiedDelay.coerceAtLeast(2).toLong())
             logDebug(TAG, "Displaying push notification in $delay second(s)")
             oneTimeWorkRequest.setInitialDelay(delay, TimeUnit.SECONDS)
         }
