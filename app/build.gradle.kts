73722a26948aeefbba790493f98e79693cf3224d
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2024-08-16 12:27:15 +0530

    Release 6.3.3 https://github.com/oxygen-updater/oxygen-updater/releases/tag/oxygen-updater-6.3.3
    - [ads] fix normal-sized banner taking up full screen height by removing support for new collapsible banner format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         minSdk = 21
         targetSdk = 34
 
-        versionCode = 117
-        versionName = "6.3.2"
+        versionCode = 118
+        versionName = "6.3.3"
 
         testApplicationId = "$namespace.$testBuildType"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
