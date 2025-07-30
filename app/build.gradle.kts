f174313a33394844a4a9d78dda918b53195db976
Adhiraj Singh Chauhan <adhirajsinghchauhan@gmail.com>
2025-07-30 21:34:44 +0530

    Release 6.7.3 https://github.com/oxygen-updater/oxygen-updater/releases/tag/oxygen-updater-6.7.3
    - [detection] support OnePlus Pad 3 regional variants
    - [icons] switched to Material Symbols
    - [device] replace default/fallback OnePlus 7 Pro image with conditional icons
    - [main] fixed "become a contributor" sheet auto-hides immediately upon open
    - [main] fixed incorrect unregistration of Play in-app update listener
    - Performance improvements & dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,8 +137,8 @@ android {
         minSdk = 21
         targetSdk = 36
 
-        versionCode = 126
-        versionName = "6.7.2"
+        versionCode = 127
+        versionName = "6.7.3"
 
         addManifestPlaceholders(mapOf("advertisingAppId" to "ca-app-pub-1816831161514116~4275332954"))
         buildConfigField("String", "AD_BANNER_MAIN_ID", "\"ca-app-pub-1816831161514116/9792024147\"")
